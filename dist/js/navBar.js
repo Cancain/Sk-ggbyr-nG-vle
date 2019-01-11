78f0ffea3d8dd1484cd2b492c1b0adb0451f5060
Cancain <eriksson.tomas@gmail.com>
2019-01-11 09:56:51 +0100

    cleaned up in the navBar script

--- dist/js/navBar.js
+++ dist/js/navBar.js
@@ -6,31 +6,38 @@ const dropDownMenuSm = document.getElementById("dropDownMenuSm");
 const contactBtnLg = document.getElementById("contactBtnLg");
 const dropDownMenuContact = document.getElementById("dropDownMenuContact");
 
-setContactDrop();
+//script variables
+let hambPressed = false;
 
-var hambPressed = false;
-
-icon.addEventListener("click", () => {
-  if (!hambPressed) {
-    hambPressed = true;
-    openMenu();
-    updateIcon();
+contactBtnLg.addEventListener("click", () => {
+  if (dropDownMenuContact.classList.contains("hidden")) {
+    dropDownMenuContact.classList.remove("hidden");
+    window.name = "open";
   } else {
-    hambPressed = false;
-    closeMenu();
-    updateIcon();
+    dropDownMenuContact.classList.add("hidden");
+    window.name = "";
   }
 });
 
+//Only shows the dropdownmenu for the contact pages or if it's clicked
+setContactDrop();
 function setContactDrop() {
+  //Sets url to the url of the current page
   let url = window.location.href;
+  //Sets currentPage to the page at the end if the url
   let currentPage = url.substr(url.lastIndexOf('/') + 1);
   let inContact = false;
+  //saves the pages to look for in the contactPages array
   let contactPages = [
     "book.php",
     "contact.php"
   ];
+  inContact = checkContactPages(contactPages, currentPage, inContact);
+}
 
+//this function loops though the contactPages array
+//hides the dropdown menu if not in any of the contact pages
+function checkContactPages(contactPages, currentPage, inContact) {
   contactPages.forEach(element => {
     if (currentPage == element) {
       inContact = true;
@@ -43,7 +50,21 @@ function setContactDrop() {
   else {
     dropDownMenuContact.classList.add("hidden");
   }
+  return inContact;
 }
+
+//Click event for the mobile menu button
+icon.addEventListener("click", () => {
+  if (!hambPressed) {
+    hambPressed = true;
+    openMenu();
+    updateIcon();
+  } else {
+    hambPressed = false;
+    closeMenu();
+    updateIcon();
+  }
+});
 
 function openMenu() {
   dropDownMenuSm.classList.remove("hidden");
@@ -61,16 +82,5 @@ function updateIcon() {
   }
 }
 
-contactBtnLg.addEventListener("click", () => {
-  if (dropDownMenuContact.classList.contains("hidden")) {
-    dropDownMenuContact.classList.remove("hidden");
-    window.name = "open";
-  } else {
-    dropDownMenuContact.classList.add("hidden");
-    window.name = "";
-  }
-});
 
-function manageContactDropDown() {
 
-}
