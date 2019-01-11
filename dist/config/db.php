391900fd6ccc52de9f376fd0771c0d04d3be489b
Cancain <eriksson.tomas@gmail.com>
2019-01-11 09:39:40 +0100

    portfolio now gets posts from the database and shows them on screen

--- dist/config/db.php
+++ dist/config/db.php
@@ -1,9 +1,20 @@
 <?php require 'config.php'?>
 
 <?php
-//setting up dsn
-$dsn = 'mysql:host=' . DB_HOST . ':dbname=' . DB_NAME;
-echo DB_HOST;
+
+//Set DSN
+$dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+
+//Create a PDO instance
+$connection = new PDO($dsn, DB_USER, DB_PASSWORD);
+$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+$sql = 'SELECT * FROM portfolio ORDER BY id DESC';
+$stmt = $connection->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll();
+
+
 
 
 ?>
--- dist/portfolio.php
+++ dist/portfolio.php
@@ -1,10 +1,23 @@
+<meta charset="utf-8">
 <?php require 'inc/header.html' ?>
 <?php require 'config/db.php' ?>
+<div class="wrapper">
+<?php foreach ($posts as $post) : ?>
 
-<?php 
+ 
+<section class="content-container">
+    <header class="content">
+        <h1><?php echo utf8_encode($post->title); ?></h1>
+        <?php echo utf8_encode($post->createdAt); ?>
+        <p><?php echo utf8_encode($post->body); ?></p>
+    </header>
+</section>
 
 
-?>
+
+
+
+<?php endforeach; ?>
+</div>
 
 <?php require 'inc/footer.html' ?>
-
