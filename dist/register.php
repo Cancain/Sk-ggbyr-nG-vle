6816192d7e146e4d1ce000c4b8c2e58a3af2e629
Cancain <eriksson.tomas@gmail.com>
2019-01-11 11:53:22 +0100

    made a page to register users

--- dist/register.php
+++ dist/register.php
@@ -1,4 +1,41 @@
 <?php require_once 'inc/header.html' ?>
 
+<div class="gridContact">
+    <div id="box1Contact">
+        <form action="" method="post">
+            <div class="formBox">
+                <label>Förnamn</label><br>
+                <input class="formField" type="text" name="">
+            </div>
+
+            <div class="formBox">
+                <label>Efternamn</label><br>
+                <input class="formField" type="text" name="">
+            </div>
+
+            <div class="formBox">
+                <label>Email</label><br>
+                <input class="formField" type="text" name="">
+            </div>
+
+            <div class="formBox">
+                <label>Användarnamn</label><br>
+                <input class="formField" type="text" name="">
+            </div>
+
+            <div class="formBox">
+                <label>Lösenord</label><br>
+                <input class="formField" type="password" name="">
+            </div>
+
+            <div class="formBox">
+                <label>Bekräfta lösenord</label><br>
+                <input class="formField" type="password" name="">
+                <button class="formBtn" type="submit" name="submit">Skicka</button>
+            </div>
+        </form>
+    </div>
+</div>
+
 
 <?php require 'inc/footer.html' ?>
