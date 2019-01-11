26aeae5ad6bf85e99cf53fca829f95780e6b31b1
Cancain <eriksson.tomas@gmail.com>
2019-01-11 13:42:16 +0100

    more work on register backend

--- dist/register.php
+++ dist/register.php
@@ -4,35 +4,35 @@
 
 <div class="gridContact">
     <div id="box1Contact">
-        <form action="" method="post">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
             <div class="formBox">
                 <label>Förnamn</label><br>
-                <input class="formField" type="text" name="">
+                <input class="formField" type="text" name="firstName">
             </div>
 
             <div class="formBox">
                 <label>Efternamn</label><br>
-                <input class="formField" type="text" name="">
+                <input class="formField" type="text" name="lastName">
             </div>
 
             <div class="formBox">
                 <label>Email</label><br>
-                <input class="formField" type="text" name="">
+                <input class="formField" type="text" name="email">
             </div>
 
             <div class="formBox">
                 <label>Användarnamn</label><br>
-                <input class="formField" type="text" name="">
+                <input class="formField" type="text" name="userName">
             </div>
 
             <div class="formBox">
                 <label>Lösenord</label><br>
-                <input class="formField" type="password" name="">
+                <input class="formField" type="password" name="passW">
             </div>
 
             <div class="formBox">
                 <label>Bekräfta lösenord</label><br>
-                <input class="formField" type="password" name="">
+                <input class="formField" type="password" name="confirmPassW">
                 <button class="formBtn" type="submit" name="submit">Skicka</button>
             </div>
         </form>
